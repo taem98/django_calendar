6a9dd41e2afdc7b332cafe1ff861e694013ff071
taemi <dmswl1002@naver.com>
2019-02-07 17:09:36 +0900

    + css, html base

--- mycalendar/urls.py
+++ mycalendar/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import event.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', event.views.calendar),
+   
 ]
